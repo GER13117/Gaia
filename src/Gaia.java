4e549cd8f93cf62a425a50d98dd19a0d33ada9a1
GER13117 <okke.warner@gmail.com>
2020-11-01 21:54:11 +0100

    added TODOs

--- src/Gaia.java
+++ src/Gaia.java
@@ -23,13 +23,14 @@ public class Gaia {
 
 //TODO: Texturen und Animationen
 //TODO: SpriteSheet Stein
-//TODO: Alte PNGs löschen und durch SpriteSheet ersetzen
 //TODO: weitere Blöcke / Materialien hinzufügen
-//TODO: Terrain: Fix chunk loading
+//TODO: Terrain: Fix chunk loading??
 //TODO: Variable für temperaturen
-//TODO: Regeln zum auswählen der texturen, fix von topLayer[x]
+//TODO: Regeln zum auswählen der texturen, (fix von topLayer[x])
 //TODO: 2. Spieler hinzufügen? oder zweiten PVP Modus
 //TODO: Menü (Möglichkeit die Lautstärke einzustellen)
 //TODO: Schlagen, bauen
 //TODO: Gegner
 //TODO: Waterblock
+//TODO: Snake Minigame
+//TODO: JavaDoc Kommentare erweitern
